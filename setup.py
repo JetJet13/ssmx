780ea6c85cfed8e0b03ce5229b50ddf99b071bfd
Johny Georges <jgeorges371@gmail.com>
2018-12-15 22:28:30 -0500

    updated setup.py with more info

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = '\n' + fh.read()
+
 setup(
     name='ssmx',
-    version='v1.0.0',
+    author="Johny Georges",
+    author_email="jgeorges371@gmail.com",
+    url="https://github.com/JetJet13/ssmx",
+    python_requires=">=2.7",
+    description="CLI tool for injecting parameters stored in AWS SSM into executables."
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='v1.0.1',
     py_modules=['ssmx'],
     download_url="https://github.com/JetJet13/ssmx/archive/v1.0.0.tar.gz",
     include_package_data=True,
